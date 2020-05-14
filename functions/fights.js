1c1b20fa13546eca4c429fe1f90314ce52e35f90
illia <illia@pdx.edu>
2020-05-14 11:25:10 -0700

    added fights functions

--- functions/fights.js
+++ functions/fights.js
@@ -30,9 +30,9 @@ exports.getAllFights = functions.https.onCall((data, context) => {
             {
                 db.db('UFC').collection('Fights')
                 .find({}, { projection: { _id: 0, pageurl: 0, f1pageurl: 0, f2pageurl: 0}})
-                .toArray(function(err, docs) {
+                .toArray(function(err, fightList) {
                     db.close();
-                    resolve(docs);  //Send back array of conferences (data)
+                    resolve(fightList);  //Send back array of conferences (data)
                 });
             }
         }) 
@@ -50,7 +50,7 @@ exports.getAllFights = functions.https.onCall((data, context) => {
  */
 exports.getFightsByFighterName = functions.https.onCall((data, context) => {
     return new Promise(async function(resolve, reject) {
-        let fighterId = data.fighterId;
+        let fighterName = data.fighterName;
         MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, db) {
             if(err) {
                 console.log("Error connecting to DB");
@@ -60,9 +60,77 @@ exports.getFightsByFighterName = functions.https.onCall((data, context) => {
             else 
             {
                 db.db('UFC').collection('Fights')
-                .findOne({"fid": fighterId}, { projection: { _id: 0, url: 0 }})
+                .find({"f1name": fighterName, "f2name": fighterName}, { projection: { _id: 0, pageurl: 0, f1pageurl: 0, f2pageurl: 0 }})
                 .then(fighter => resolve(fighter));
             }
         }) 
     });
 })
+
+/**
+ * Function connects to db and fetches full Fighters collection
+ * 
+ * @returns {Promise} - returns a list of all fighters from Fighters collection
+ */
+exports.getAllMethodCounts = functions.https.onCall((data, context) => {
+    return new Promise(async function(resolve, reject) {
+        MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, db) {
+            if(err) {
+                console.log("Error connecting to DB");
+                db.close();
+                return reject('ERROR: ' + err);
+            }
+            else 
+            {
+                db.db('UFC').collection('Fights')
+                .find({}, { projection: { _id: 0, pageurl: 0, f1pageurl: 0, f2pageurl: 0}})
+                .toArray(function(err, fightList) {
+                    byMethod = count(fightList, function(item) {
+                        return item.method;
+                    });
+                    db.close();
+                    resolve(byMethod);  
+                });
+            }
+        }) 
+    })
+});
+
+/**
+ * Function connects to db and fetches full Fighters collection
+ * 
+ * @returns {Promise} - returns a list of all fighters from Fighters collection
+ */
+exports.getAllMethodDefinitionCounts = functions.https.onCall((data, context) => {
+    return new Promise(async function(resolve, reject) {
+        MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, db) {
+            if(err) {
+                console.log("Error connecting to DB");
+                db.close();
+                return reject('ERROR: ' + err);
+            }
+            else 
+            {
+                db.db('UFC').collection('Fights')
+                .find({}, { projection: { _id: 0, pageurl: 0, f1pageurl: 0, f2pageurl: 0}})
+                .toArray(function(err, fightList) {
+                    byMethod = count(fightList, function(item) {
+                        return item.method_d;
+                    });
+                    db.close();
+                    resolve(byMethod);  
+                });
+            }
+        }) 
+    })
+});
+
+count = function (ary, classifier) {
+    classifier = classifier || String;
+    return ary.reduce(function (counter, item) {
+        var p = classifier(item);
+        counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
+        return counter;
+    }, {})
+};
+
